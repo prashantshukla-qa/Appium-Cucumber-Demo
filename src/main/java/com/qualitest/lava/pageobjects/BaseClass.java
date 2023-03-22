6970dae2f0fdde60514b2b149853c34ab9718aef
Prashant Shukla <prashant.shukla@qualitestgroup.com>
2023-03-22 11:37:06 +0530

    initial commit

---
+++ src/main/java/com/qualitest/lava/pageobjects/BaseClass.java
@@ -0,0 +1,29 @@
+package com.qualitest.lava.pageobjects;
+
+
+
+public class BaseClass {
+
+    
+
+    // public BaseClass() {
+    //     this.driver = new ChromeDriver();
+    // }
+
+    // public WebDriver getDriver() {
+    //     return this.driver;
+    // }
+
+    // public void launchApplication() {
+    //     launchApplication("https://the-internet.herokuapp.com/");
+    // }
+
+    // public void launchApplication(String url) {
+    //     this.driver.get(url);
+    // }    
+
+    // public void closeTestSession() {
+    //     this.driver.quit();
+    // }
+    
+}
---
+++ src/test/java/com/qualitest/lava/StepDefs.java
@@ -0,0 +1,45 @@
+package com.qualitest.lava;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs {
+
+    @Given("I am on the application Landing Page")
+    public void i_am_on_the_application_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I Navigate to the Sign In Page")
+    public void i_navigate_to_the_sign_in_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter the username as {string}")
+    public void i_enter_the_username_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter the password as {string}")
+    public void i_enter_the_password_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click the Sign In Button")
+    public void i_click_the_sign_in_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see the message {string}")
+    public void i_should_see_the_message(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
